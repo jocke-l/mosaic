c64dcdd309cfa06301f4bc8156dfcef944835a63
Joakim Saario <saario.joakim@gmail.com>
2016-12-25 23:14:32 +0100

    Multiprocessing

--- mosaic/builder.py
+++ mosaic/builder.py
@@ -1,3 +1,5 @@
+import itertools
+import multiprocessing
 import os
 import random
 import re
@@ -28,22 +30,34 @@ def get_best_matching_tile(tiles, color):
     )[0]])
 
 
+def get_tile_thumb(tiles, data_dir, img, pos):
+    x, y = pos
+    pixels = numpy.array(img.crop((x, y, x+20, y+20)).getdata())
+    mean_color = tuple(numpy.mean(pixels, axis=0))
+
+    tile_filename = get_best_matching_tile(tiles, mean_color)
+    tile = Image.open(os.path.join(data_dir, tile_filename))
+    return tile.resize((20, 20))
+
+
 def build(image, data_dir, output):
     img = Image.open(image)
-    tiles = get_tiles(os.listdir(data_dir))
 
     width, height = img.size
-    new_width = round(width / 20) * 20
-    new_height = round(height / 20) * 20
-    new_img = Image.new('RGB', (new_width, new_height))
-    new_img.paste(img, (0, 0, width, height))
-    for x in range(0, width, 20):
-        for y in range(0, height, 20):
-            pixels = numpy.array(img.crop((x, y, x+20, y+20)).getdata())
-            mean_color = tuple(numpy.mean(pixels, axis=0))
+    mosaic_width = round(width / 20) * 20
+    mosaic_height = round(height / 20) * 20
+    mosaic = Image.new('RGB', (mosaic_width, mosaic_height))
+    mosaic.paste(img, (0, 0, width, height))
 
-            tile_filename = get_best_matching_tile(tiles, mean_color)
-            tile = Image.open(os.path.join(data_dir, tile_filename))
-            new_img.paste(tile.resize((20, 20)), (x, y))
+    positions = list(itertools.product(range(0, width, 20),
+                                       range(0, height, 20)))
 
-    new_img.save(output)
+    tiles = get_tiles(os.listdir(data_dir))
+
+    with multiprocessing.Pool(processes=4) as pool:
+        tile_thumbs = pool.map(partial(get_tile_thumb, tiles, data_dir, img),
+                               positions)
+
+    for tile, pos in zip(tile_thumbs, positions):
+        mosaic.paste(tile, pos)
+    mosaic.save(output)
